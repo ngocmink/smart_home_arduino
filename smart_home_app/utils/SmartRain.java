4f048d6e4afd7e2379fa2be9dd18580046470941
hustsak <viraksakkoy8@gmail.com>
2025-11-08 21:11:04 +0700

    Add SmartRain class with control methods
    Implemented methods to control SmartRain status.

--- smart_home_app/utils/SmartRain.java
+++ smart_home_app/utils/SmartRain.java
@@ -1,5 +1,23 @@
 package utils;
 
 public class SmartRain {
+    private boolean status;
     
+    public SmartRain() {
+        this.status = false;
+    }
+    
+    public void turnOn() {
+        SerialCommunication.sendCommand("SmartRain on");
+        this.status = true;
+    }
+    
+    public void turnOff() {
+        SerialCommunication.sendCommand("SmartRain off");
+        this.status = false;
+    }
+    
+    public boolean isOn() {
+        return this.status;
+    }
 }
