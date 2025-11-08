3f623101eb603c6459e821fed86601ace5c84e0b
hustsak <viraksakkoy8@gmail.com>
2025-11-08 21:37:55 +0700

    Update SmartRain.java
    Fixed method name and improved @Override usage in SmartRain class

--- smart_home_app/utils/SmartRain.java
+++ smart_home_app/utils/SmartRain.java
@@ -1,5 +1,34 @@
 package utils;
 
-public class SmartRain {
+import java.io.IOException;
+import com.fazecast.jSerialComm.SerialPort;
+
+public class SmartRain implements SmartDevice {
+    private boolean status;
     
+    public SmartRain() {
+        this.status = false;
+    }
+    
+    @Override
+    public void turnOn(SerialPort sp) throws IOException {
+        sp.writeBytes("SmartRain on".getBytes(), "SmartRain on".length());
+        this.status = true;
+    }
+    
+    @Override
+    public void turnOff(SerialPort sp) throws IOException {
+        sp.writeBytes("SmartRain off".getBytes(), "SmartRain off".length());
+        this.status = false;
+    }
+    
+    @Override
+    public boolean isOn() {
+        return this.status;
+    }
+    
+    @Override
+    public void name(String name) {
+        // Implementation if needed
+    }
 }
