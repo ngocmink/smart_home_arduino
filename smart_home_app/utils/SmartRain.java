8a672a4eb47e23603e7a1bd9cbb3371aac3f6e87
hustsak <viraksakkoy8@gmail.com>
2025-11-13 16:46:49 +0700

    Implement SmartRain class with device control methods

--- smart_home_app/utils/SmartRain.java
+++ smart_home_app/utils/SmartRain.java
@@ -1,5 +1,36 @@
 package utils;
 
-public class SmartRain {
+import java.io.IOException;
+import com.fazecast.jSerialComm.SerialPort;
+
+public class SmartRain implements SmartDevice {
+    private boolean on;
+    private String name;
     
+    @Override
+    public void turnOn(SerialPort sp) throws IOException {
+        on = true;
+        sp.getOutputStream().write("RAIN_ON\n".getBytes());
+        sp.getOutputStream().flush();
+        System.out.println("SmartRain: RainDetector turned on.");
+    }
+    
+    @Override
+    public void turnOff(SerialPort sp) throws IOException {
+        on = false;
+        sp.getOutputStream().write("RAIN_OFF\n".getBytes());
+        sp.getOutputStream().flush();
+        System.out.println("SmartRain: RainDetector turned off.");
+    }
+    
+    @Override
+    public boolean isOn() {
+        return on;
+    }
+    
+    @Override
+    public void name(String name) {
+        this.name = name;
+        System.out.println("Add device: " + this.name);
+    }
 }
